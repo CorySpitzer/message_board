b2b4463e961e0b8262b9cb40bcc2880adb14c46e
Cory Spitzer <coryspitzer@gmail.com>
2015-06-12 16:10:29 -0700

    Allow users to edit answers

--- controllers/question.js
+++ controllers/question.js
@@ -1,9 +1,18 @@
 // We called the action 'delete' in question.hbs
 // so we need to define it (We need an ObjectController,
 // as opposed to just a Controller, so that we can display
-// a question's properties on its handlebars page):
+// a question's properties on its handlebars page because
+// ObjectControllers give their hbs files access to properties
+// and Controllers don't):
 MessageBoard.QuestionController = Ember.ObjectController.extend({
+  isediting: false,
   actions: {
+    edit: function() {
+      this.set('isEditing', true)
+    },
+    save: function() {
+      this.set('isEditing', false)
+    },
     delete: function() {
       if (confirm('Really?')) {
         this.get('model').destroyRecord();
