ca42b5f98ad2de983191e36f2cc9d4e622860d70
Cory Spitzer <coryspitzer@gmail.com>
2015-06-12 14:09:55 -0700

    Succesfully use add question form

--- controllers/new-question.js
+++ controllers/new-question.js
@@ -3,11 +3,16 @@
 MessageBoard.NewQuestionController = Ember.Controller.extend({
   actions: {
     ask: function() {
+      // Create a DS store record
       var newQuestion = this.store.createRecord('question', {
         question: this.get('question'),
         author: this.get('author'),
         description: this.get('description')
       });
+      // Save the record so that it stays in local memory
+      newQuestion.save();
+      // Leave the page we just submitted the form from
+      this.transitionToRoute('questions');
     }
   }
 });
--- router.js
+++ router.js
@@ -2,6 +2,7 @@ MessageBoard.Router.map(function() {
   // Route '/' to the questions.hbs page with the
   // questions.js route available to the questions.hbs:
   this.resource('questions', {path: '/'});
+  this.resource('question', {path: ':question_id'})
   this.resource('new-question');
   // Route a specific post id to the 'post' page:
   // this.resource('question', {path: ':post_id'})
