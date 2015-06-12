0ec06b2396ca25328e72f9db774d568908c15435
Cory Spitzer <coryspitzer@gmail.com>
2015-06-12 15:36:34 -0700

    Display a question's properties and stay on its page if deletion is declined

--- controllers/question.js
+++ controllers/question.js
@@ -1,13 +1,14 @@
 // We called the action 'delete' in question.hbs
-// so we need to define it (not sure why it is an
-// Object controller instead of a regular one):
-MessageBoard.QuestionController = Ember.Controller.extend({
+// so we need to define it (We need an ObjectController,
+// as opposed to just a Controller, so that we can display
+// a question's properties on its handlebars page):
+MessageBoard.QuestionController = Ember.ObjectController.extend({
   actions: {
     delete: function() {
       if (confirm('Really?')) {
         this.get('model').destroyRecord();
+        this.transitionToRoute('/', {async: true})
       }
-    this.transitionToRoute('/', {async: true})
     }
   }
 });
